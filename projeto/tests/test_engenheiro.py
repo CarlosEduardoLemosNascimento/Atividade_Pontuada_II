a4da255f24c98c0aa174743a8b8a6a0add9de45a
CarlosEduardoLemosNascimento <159453764+CarlosEduardoLemosNascimento@users.noreply.github.com>
2024-10-07 12:46:04 +0000

    .

--- projeto/tests/test_engenheiro.py
+++ projeto/tests/test_engenheiro.py
@@ -13,7 +13,7 @@ class TestEngenheiro(unittest.TestCase):
     def test_engenheiro_str(self):
         endereco = Endereco("Rua A", "123", "Apt 1", "12345-678", "Bahia")
         engenheiro = Engenheiro("João", "71999999999", "joao@exemplo.com", endereco, "123456")
-        self.assertEqual(str(engenheiro), "Engenheiro: Nome: João, Telefone: 71999999999, Email: joao@exemplo.com, Endereço: Rua A, 123 Apt 1, Bahia - 12345-678, CREA: 123456, Salário: 8000.0")
+        self.assertEqual(str(engenheiro), "Engenheiro: Nome: João, Telefone: 71999999999, Email: joao@exemplo.com, Endereço: Rua A, 123, Apt 1, Bahia - 12345-678, CREA: 123456, Salário: 8000.0")
 
 if __name__ == '__main__':
     unittest.main()
