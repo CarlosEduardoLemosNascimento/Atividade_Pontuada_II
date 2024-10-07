ee2e3c68766f6e9701041d625d5a4062808bec91
CarlosEduardoLemosNascimento <159453764+CarlosEduardoLemosNascimento@users.noreply.github.com>
2024-10-07 12:49:11 +0000

    .

--- projeto/tests/test_medico.py
+++ projeto/tests/test_medico.py
@@ -13,7 +13,7 @@ class TestMedico(unittest.TestCase):
     def test_medico_str(self):
         endereco = Endereco("Rua A", "123", "Apt 1", "12345-678", "Bahia")
         medico = Medico("Maria", "71988888888", "maria@exemplo.com", endereco, "654321")
-        self.assertEqual(str(medico), "Médico: Nome: Maria, Telefone: 71988888888, Email: maria@exemplo.com, Endereço: Rua A, 123 Apt 1, Bahia - 12345-678, CRM: 654321, Salário: 10000.0")
+        self.assertEqual(str(medico), "Médico: Nome: Maria, Telefone: 71988888888, Email: maria@exemplo.com, Endereço: Rua A, 123, Apt 1, Bahia - 12345-678, CRM: 654321, Salário: 10000.0")
 
 if __name__ == '__main__':
     unittest.main()
