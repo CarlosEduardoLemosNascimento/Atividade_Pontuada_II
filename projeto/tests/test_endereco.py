1b27878532ec28d8f103b19aada13150797f0d78
CarlosEduardoLemosNascimento <159453764+CarlosEduardoLemosNascimento@users.noreply.github.com>
2024-10-07 12:44:20 +0000

    .

--- projeto/tests/test_endereco.py
+++ projeto/tests/test_endereco.py
@@ -3,16 +3,16 @@ from projeto.models.endereco import Endereco
 
 class TestEndereco(unittest.TestCase):
     def test_endereco_creation(self):
-        endereco = Endereco("Rua A", "123", "Apt 1", "12345-678", "São Paulo")
+        endereco = Endereco("Rua A", "123", "Apt 1", "12345-678", "Bahia")
         self.assertEqual(endereco.logradouro, "Rua A")
         self.assertEqual(endereco.numero, "123")
         self.assertEqual(endereco.complemento, "Apt 1")
         self.assertEqual(endereco.cep, "12345-678")
-        self.assertEqual(endereco.cidade, "São Paulo")
+        self.assertEqual(endereco.cidade, "Bahia")
 
     def test_endereco_str(self):
         endereco = Endereco("Rua A", "123", "Apt 1", "12345-678", "São Paulo")
-        self.assertEqual(str(endereco), "Rua A, 123 Apt 1, São Paulo - 12345-678")
+        self.assertEqual(str(endereco), "Rua A, 123, Apt 1, São Paulo - 12345-678")
 
 if __name__ == '__main__':
     unittest.main()
